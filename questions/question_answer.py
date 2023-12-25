2a10aeb33d50692f44dd7ca00fbe97deb16b06fa
MmdSparrow <esmirk.137@gmail.com>
2023-12-26 01:06:11 +0330

    refactor

--- questions/question_answer.py
+++ questions/question_answer.py
@@ -10,11 +10,12 @@ def parsivar_stem(word, word_pos=None):
 def hazm_stem(word):
     return Stemmer().stem(word)
 
+print('parsiavr:')
 print(parsivar_stem('می خواهد'))
 print(parsivar_stem('میخواهد'))
 print(parsivar_stem('میخواهد برود'))
-
-
+print()
+print('hazm:')
 print(hazm_stem('می خواهد'))
 print(hazm_stem('میخواهد'))
 print(hazm_stem('میخواهد برود'))
